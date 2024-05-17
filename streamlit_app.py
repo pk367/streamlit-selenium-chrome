9ab236e8b99d8f7214590fb42955540c269260d6
pk367 <154593937+pk367@users.noreply.github.com>
2024-05-17 13:54:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service 
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager 
 from webdriver_manager.utils import ChromeType
 
 @st.cache(allow_output_mutation=True)
